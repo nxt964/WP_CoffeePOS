d0bf8d9d6239db4d0c3e6446341a091961bcd4d6
nban22 <nban22@clc.fitus.edu.vn>
2025-04-19 11:20:56 +0700

    #56-nban22-Display ingredients on the product detail page

--- CoffeePOS.Core/Repositories/SqliteManualRepository.cs
+++ CoffeePOS.Core/Repositories/SqliteManualRepository.cs
@@ -1,173 +1,173 @@
-﻿// Core/Repositories/SqliteManualRepository.cs
-using CoffeePOS.Core.Data;
-using CoffeePOS.Core.Interfaces;
-using Microsoft.Data.Sqlite;
-using Humanizer;
-using System;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Threading.Tasks;
+﻿    // Core/Repositories/SqliteManualRepository.cs
+    using CoffeePOS.Core.Data;
+    using CoffeePOS.Core.Interfaces;
+    using Microsoft.Data.Sqlite;
+    using Humanizer;
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Threading.Tasks;
 
-namespace CoffeePOS.Core.Repositories;
+    namespace CoffeePOS.Core.Repositories;
 
-public class SqliteManualRepository<T> : IRepository<T> where T : class
-{
-    private readonly SqliteConnectionFactory _connectionFactory;
-
-    public SqliteManualRepository(SqliteConnectionFactory connectionFactory)
+    public class SqliteManualRepository<T> : IRepository<T> where T : class
     {
-        _connectionFactory = connectionFactory;
-    }
+        private readonly SqliteConnectionFactory _connectionFactory;
 
-    public async Task<IEnumerable<T>> GetAll()
-    {
-        var result = new List<T>();
-        var tableName = typeof(T).Name.Pluralize();
-
-        using var connection = _connectionFactory.CreateConnection();
-        await connection.OpenAsync();
-
-        var command = connection.CreateCommand();
-        command.CommandText = $"SELECT * FROM {tableName}";
-
-        using var reader = await command.ExecuteReaderAsync();
-        while (await reader.ReadAsync())
+        public SqliteManualRepository(SqliteConnectionFactory connectionFactory)
         {
-            var entity = MapToEntity(reader);
-            result.Add(entity);
+            _connectionFactory = connectionFactory;
         }
 
-        return result;
-    }
-
-    public async Task<T> GetById(int id)
-    {
-        var tableName = typeof(T).Name.Pluralize();
-
-        using var connection = _connectionFactory.CreateConnection();
-        await connection.OpenAsync();
-
-        var command = connection.CreateCommand();
-        command.CommandText = $"SELECT * FROM {tableName} WHERE Id = @Id";
-        command.Parameters.AddWithValue("@Id", id);
-
-        using var reader = await command.ExecuteReaderAsync();
-        if (await reader.ReadAsync())
+        public async Task<IEnumerable<T>> GetAll()
         {
-            return MapToEntity(reader);
-        }
-        return null;
-    }
+            var result = new List<T>();
+            var tableName = typeof(T).Name.Pluralize();
 
-    public async Task<T> Add(T entity)
-    {
-        var tableName = typeof(T).Name.Pluralize();
-        var properties = typeof(T).GetProperties().Where(p => p.Name != "Id"); // Bỏ Id nếu tự tăng
-        var columnNames = string.Join(", ", properties.Select(p => p.Name));
-        var paramNames = string.Join(", ", properties.Select(p => $"@{p.Name}"));
+            using var connection = _connectionFactory.CreateConnection();
+            await connection.OpenAsync();
 
-        using var connection = _connectionFactory.CreateConnection();
-        await connection.OpenAsync();
+            var command = connection.CreateCommand();
+            command.CommandText = $"SELECT * FROM {tableName}";
 
-        var command = connection.CreateCommand();
-        command.CommandText = $"INSERT INTO {tableName} ({columnNames}) VALUES ({paramNames}) RETURNING *";
-
-        foreach (var prop in properties)
-        {
-            command.Parameters.AddWithValue($"@{prop.Name}", prop.GetValue(entity) ?? DBNull.Value);
-        }
-
-        using var reader = await command.ExecuteReaderAsync();
-        if (await reader.ReadAsync())
-        {
-            return MapToEntity(reader);
-        }
-        return entity;
-    }
-
-    public async Task<T> Update(T entity)
-    {
-        var tableName = typeof(T).Name.Pluralize();
-        var properties = typeof(T).GetProperties().Where(p => p.Name != "Id");
-        var setClause = string.Join(", ", properties.Select(p => $"{p.Name} = @{p.Name}"));
-        var id = (int)typeof(T).GetProperty("Id").GetValue(entity);
-
-        using var connection = _connectionFactory.CreateConnection();
-        await connection.OpenAsync();
-
-        var command = connection.CreateCommand();
-        command.CommandText = $"UPDATE {tableName} SET {setClause} WHERE Id = @Id RETURNING *";
-        command.Parameters.AddWithValue("@Id", id);
-
-        foreach (var prop in properties)
-        {
-            command.Parameters.AddWithValue($"@{prop.Name}", prop.GetValue(entity) ?? DBNull.Value);
-        }
-
-        using var reader = await command.ExecuteReaderAsync();
-        if (await reader.ReadAsync())
-        {
-            return MapToEntity(reader);
-        }
-        return entity;
-    }
-
-    public async Task<T> Delete(int id)
-    {
-        var tableName = typeof(T).Name.Pluralize();
-
-        using var connection = _connectionFactory.CreateConnection();
-        await connection.OpenAsync();
-
-        var command = connection.CreateCommand();
-        command.CommandText = $"DELETE FROM {tableName} WHERE Id = @Id RETURNING *";
-        command.Parameters.AddWithValue("@Id", id);
-
-        using var reader = await command.ExecuteReaderAsync();
-        if (await reader.ReadAsync())
-        {
-            return MapToEntity(reader);
-        }
-        return null;
-    }
-
-    protected T MapToEntity(SqliteDataReader reader)
-    {
-        var entity = Activator.CreateInstance<T>();
-        var properties = typeof(T).GetProperties();
-
-        foreach (var prop in properties)
-        {
-            if (reader.HasColumn(prop.Name) && !reader.IsDBNull(reader.GetOrdinal(prop.Name)))
+            using var reader = await command.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
             {
-                var value = reader.GetValue(reader.GetOrdinal(prop.Name));
-
-                // Nếu thuộc tính là Nullable<T>, ta cần lấy kiểu thực tế bên trong
-                var targetType = Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType;
-
-                // Chuyển đổi giá trị sang kiểu phù hợp
-                var convertedValue = Convert.ChangeType(value, targetType);
-
-                prop.SetValue(entity, convertedValue);
+                var entity = MapToEntity(reader);
+                result.Add(entity);
             }
+
+            return result;
         }
 
-        return entity;
-    }
-
-}
-
-public static class SqliteDataReaderExtensions
-{
-    public static bool HasColumn(this SqliteDataReader reader, string columnName)
-    {
-        for (int i = 0; i < reader.FieldCount; i++)
+        public async Task<T> GetById(int id)
         {
-            if (reader.GetName(i).Equals(columnName, StringComparison.OrdinalIgnoreCase))
+            var tableName = typeof(T).Name.Pluralize();
+
+            using var connection = _connectionFactory.CreateConnection();
+            await connection.OpenAsync();
+
+            var command = connection.CreateCommand();
+            command.CommandText = $"SELECT * FROM {tableName} WHERE Id = @Id";
+            command.Parameters.AddWithValue("@Id", id);
+
+            using var reader = await command.ExecuteReaderAsync();
+            if (await reader.ReadAsync())
             {
-                return true;
+                return MapToEntity(reader);
             }
+            return null;
         }
-        return false;
+
+        public async Task<T> Add(T entity)
+        {
+            var tableName = typeof(T).Name.Pluralize();
+            var properties = typeof(T).GetProperties().Where(p => p.Name != "Id"); // Bỏ Id nếu tự tăng
+            var columnNames = string.Join(", ", properties.Select(p => p.Name));
+            var paramNames = string.Join(", ", properties.Select(p => $"@{p.Name}"));
+
+            using var connection = _connectionFactory.CreateConnection();
+            await connection.OpenAsync();
+
+            var command = connection.CreateCommand();
+            command.CommandText = $"INSERT INTO {tableName} ({columnNames}) VALUES ({paramNames}) RETURNING *";
+
+            foreach (var prop in properties)
+            {
+                command.Parameters.AddWithValue($"@{prop.Name}", prop.GetValue(entity) ?? DBNull.Value);
+            }
+
+            using var reader = await command.ExecuteReaderAsync();
+            if (await reader.ReadAsync())
+            {
+                return MapToEntity(reader);
+            }
+            return entity;
+        }
+
+        public async Task<T> Update(T entity)
+        {
+            var tableName = typeof(T).Name.Pluralize();
+            var properties = typeof(T).GetProperties().Where(p => p.Name != "Id");
+            var setClause = string.Join(", ", properties.Select(p => $"{p.Name} = @{p.Name}"));
+            var id = (int)typeof(T).GetProperty("Id").GetValue(entity);
+
+            using var connection = _connectionFactory.CreateConnection();
+            await connection.OpenAsync();
+
+            var command = connection.CreateCommand();
+            command.CommandText = $"UPDATE {tableName} SET {setClause} WHERE Id = @Id RETURNING *";
+            command.Parameters.AddWithValue("@Id", id);
+
+            foreach (var prop in properties)
+            {
+                command.Parameters.AddWithValue($"@{prop.Name}", prop.GetValue(entity) ?? DBNull.Value);
+            }
+
+            using var reader = await command.ExecuteReaderAsync();
+            if (await reader.ReadAsync())
+            {
+                return MapToEntity(reader);
+            }
+            return entity;
+        }
+
+        public async Task<T> Delete(int id)
+        {
+            var tableName = typeof(T).Name.Pluralize();
+
+            using var connection = _connectionFactory.CreateConnection();
+            await connection.OpenAsync();
+
+            var command = connection.CreateCommand();
+            command.CommandText = $"DELETE FROM {tableName} WHERE Id = @Id RETURNING *";
+            command.Parameters.AddWithValue("@Id", id);
+
+            using var reader = await command.ExecuteReaderAsync();
+            if (await reader.ReadAsync())
+            {
+                return MapToEntity(reader);
+            }
+            return null;
+        }
+
+        protected T MapToEntity(SqliteDataReader reader)
+        {
+            var entity = Activator.CreateInstance<T>();
+            var properties = typeof(T).GetProperties();
+
+            foreach (var prop in properties)
+            {
+                if (reader.HasColumn(prop.Name) && !reader.IsDBNull(reader.GetOrdinal(prop.Name)))
+                {
+                    var value = reader.GetValue(reader.GetOrdinal(prop.Name));
+
+                    // Nếu thuộc tính là Nullable<T>, ta cần lấy kiểu thực tế bên trong
+                    var targetType = Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType;
+
+                    // Chuyển đổi giá trị sang kiểu phù hợp
+                    var convertedValue = Convert.ChangeType(value, targetType);
+
+                    prop.SetValue(entity, convertedValue);
+                }
+            }
+
+            return entity;
+        }
+
     }
-}
+
+    public static class SqliteDataReaderExtensions
+    {
+        public static bool HasColumn(this SqliteDataReader reader, string columnName)
+        {
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                if (reader.GetName(i).Equals(columnName, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
--- CoffeePOS/Views/ProductsDetailPage.xaml.cs
+++ CoffeePOS/Views/ProductsDetailPage.xaml.cs
@@ -2,7 +2,6 @@
 using CoffeePOS.Contracts.Services;
 using CoffeePOS.Core.Models;
 using CoffeePOS.ViewModels;
-
 using CommunityToolkit.WinUI.UI.Animations;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
@@ -20,12 +19,14 @@ public sealed partial class ProductsDetailPage : Page
     public ProductsDetailPage()
     {
         ViewModel = App.GetService<ProductsDetailViewModel>();
+        this.DataContext = ViewModel;
         InitializeComponent();
     }
 
     protected override void OnNavigatedTo(NavigationEventArgs e)
     {
         base.OnNavigatedTo(e);
+        ViewModel.OnNavigatedTo(e.Parameter);
     }
 
     protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
@@ -34,7 +35,6 @@ public sealed partial class ProductsDetailPage : Page
         if (e.NavigationMode == NavigationMode.Back)
         {
             var navigationService = App.GetService<INavigationService>();
-
             if (ViewModel.Item != null)
             {
                 navigationService.SetListDataItemForNextConnectedAnimation(ViewModel.Item);
@@ -44,12 +44,44 @@ public sealed partial class ProductsDetailPage : Page
 
     private void OnProductClicked(object sender, RoutedEventArgs e)
     {
-        if (sender is Button button)
+        Debug.WriteLine("[DEBUG] ProductsDetailPage.OnProductClicked: Clicked on related product");
+        if (sender is Button button && button.Tag is int productId)
         {
-            if (button.Tag is int productId)
-            {
-                ViewModel.OnProductClicked(productId);
-            }
+            ViewModel.OnProductClicked(productId);
+        }
+    }
+
+    private void EditProduct_Click(object sender, RoutedEventArgs e)
+    {
+        Debug.WriteLine("[DEBUG] ProductsDetailPage.EditProduct_Click: Editing product");
+        ViewModel.EditProductCommand.Execute(null);
+    }
+
+    private void DeleteProduct_Click(object sender, RoutedEventArgs e)
+    {
+        Debug.WriteLine("[DEBUG] ProductsDetailPage.DeleteProduct_Click: Deleting product");
+        ViewModel.DeleteProductCommand.Execute(null);
+    }
+
+    private void BackButton_Click(object sender, RoutedEventArgs e)
+    {
+        Debug.WriteLine("[DEBUG] ProductsDetailPage.BackButton_Click: Navigating back");
+        var navigationService = App.GetService<INavigationService>();
+        navigationService.GoBack();
+    }
+
+    private void AddIngredient_Click(object sender, RoutedEventArgs e)
+    {
+        Debug.WriteLine("[DEBUG] ProductsDetailPage.AddIngredient_Click: Adding ingredient");
+        ViewModel.AddIngredientCommand.Execute(null);
+    }
+
+    private void RemoveIngredient_Click(object sender, RoutedEventArgs e)
+    {
+        Debug.WriteLine("[DEBUG] ProductsDetailPage.RemoveIngredient_Click: Removing ingredient");
+        if (sender is Button button && button.Tag is ProductIngredientDetail ingredientDetail)
+        {
+            ViewModel.RemoveIngredientCommand.Execute(ingredientDetail);
         }
     }
 }
